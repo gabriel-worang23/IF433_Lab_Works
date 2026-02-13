756d39993e04a1eeb7748506f63bc0b00bb6a81c
Gabriel Worang <gabriel.nathanael@student.umn.ac.id>
2026-02-13 23:24:31 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000106290_GabrielNathanaelWorang/week01/CircleCalculator.kt
+++ src/oop_00000106290_GabrielNathanaelWorang/week01/CircleCalculator.kt
@@ -1,14 +1,12 @@
 package oop_00000106290_GabrielNathanaelWorang.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
-    // Calculation
+
     val area = pi * radius * radius
-    // Output Concatenation
+
     println("Radius: $radius Area: $area")
-    // Logic check
     println(checkSize(area))
 }
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
