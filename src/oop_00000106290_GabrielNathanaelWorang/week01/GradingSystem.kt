fcc8ebc72d523e1f59493aea1747f7b643bae747
Gabriel Worang <gabriel.nathanael@student.umn.ac.id>
2026-02-13 10:59:05 +0700

    “week01: refactor to use val and string templates”

--- src/oop_00000106290_GabrielNathanaelWorang/week01/GradingSystem.kt
+++ src/oop_00000106290_GabrielNathanaelWorang/week01/GradingSystem.kt
@@ -1,8 +1,8 @@
 package oop_00000106290_GabrielNathanaelWorang.week01
 
 fun main() {
-    var name: String + "John Thor"
-    var score: Int = 80
+    var name = "John Thor"
+    var score = 80
 
-    println("Nama: " + name + ". Nilai: " + score)
+    println("Nama: $name, Nilai: $score")
 }
