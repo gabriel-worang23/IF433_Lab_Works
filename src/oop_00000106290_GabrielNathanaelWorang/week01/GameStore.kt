65d7cf72fd4a59f03187401ad45d4f55ee993224
Gabriel Worang <gabriel.nathanael@student.umn.ac.id>
2026-02-13 23:28:33 +0700

    week01: (tugas 2) setup GameStore file

---
+++ src/oop_00000106290_GabrielNathanaelWorang/week01/GameStore.kt
@@ -0,0 +1,8 @@
+package oop_00000106290_GabrielNathanaelWorang.week01
+
+fun main() {
+    val gameTitle: String = "Elden Ring: Shadow of the Erdtree"
+    val price: Int = 599000
+
+    println("Data Game Berhasil Disiapkan: $gameTitle dengan harga Rp $price")
+}
