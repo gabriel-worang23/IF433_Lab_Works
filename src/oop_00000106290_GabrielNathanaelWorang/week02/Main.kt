e23b0b9c0ac22e2712c802048a270542e9f76067
Gabriel Worang <gabriel.nathanael@student.umn.ac.id>
2026-02-14 01:16:33 +0700

    "week02: implement selection logic for constructors"

--- src/oop_00000106290_GabrielNathanaelWorang/week02/Main.kt
+++ src/oop_00000106290_GabrielNathanaelWorang/week02/Main.kt
@@ -5,7 +5,7 @@ import java.util.Scanner
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    print("--- APLIKASI PMB UMN ---")
+    println("--- APLIKASI PMB UMN ---")
 
     print("Masukkan Nama: ")
     val name = scanner.nextLine()
@@ -13,15 +13,19 @@ fun main() {
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
 
+    print("Pilih Jalur (1. Reguler, 2. Umum): ")
+    val type = scanner.next()
     scanner.nextLine()
 
-    if (nim.length != 5) {
-        println("ERROR: Pendaftaran dibatalkan, NIM harus 5 karakter!")
-    } else {
-        print("Masukkan Jurusan: ")
+    if (type == "1") {
+        print("Masukkan Jurusan : ")
         val major = scanner.nextLine()
-
-        val s1 = Student(name, nim, major)
-        println("Status: Pendaftaran Selesai.")
+        var s1 = Student(name, nim, major)
+        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+    } else if (type == "2") {
+        val s2 = Student(name, nim)
+        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+    } else {
+        println("Pilihan ngawur, pendaftaran batal!")
     }
 }
