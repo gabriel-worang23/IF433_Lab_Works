31006df5f31497da2e86726c6d738d59734e93af
Gabriel Worang <gabriel.nathanael@student.umn.ac.id>
2026-02-20 11:29:24 +0700

    "week03: (task 2) create Player class with private xp"

---
+++ src/oop_00000106290_GabrielNathanaelWorang/week03/TugasMandiri2/Player.kt
@@ -0,0 +1,22 @@
+package oop_00000106290_GabrielNathanaelWorang.week03.TugasMandiri2
+
+class Player(val username: String) {
+    var xp: Int = 0
+        private set
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount > 0) {
+            val levelSebelumnya = level
+            xp += amount
+
+            if (level > levelSebelumnya) {
+                println("Level Up! Selamat $username naik ke level $level")
+            }
+        } else {
+            println("Jumlah XP harus positif!")
+        }
+    }
+}
