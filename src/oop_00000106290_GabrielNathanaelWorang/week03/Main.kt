1e59c5bebce22bb1b7fd0216add3b6822a5a114a
Gabriel Worang <gabriel.nathanael@student.umn.ac.id>
2026-02-20 11:05:31 +0700

    "week03: test all encapsulation logic in main"

--- src/oop_00000106290_GabrielNathanaelWorang/week03/Main.kt
+++ src/oop_00000106290_GabrielNathanaelWorang/week03/Main.kt
@@ -2,6 +2,16 @@ package oop_00000106290_GabrielNathanaelWorang.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+
+    //1. Test validasi salary
+    e.salary = -1000 //harusnya print error
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    //t2. test encapsulation
+    e.increasePerformance()
+    //e.performanceRating = 5 //cobba uncomment, pasti merah(Error)
+
+    //3. Test Computes Property
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
