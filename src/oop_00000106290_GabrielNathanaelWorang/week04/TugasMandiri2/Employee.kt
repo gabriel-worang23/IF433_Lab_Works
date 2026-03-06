17ba116a831dd5a0f16fb3b135d97bc35faff1e6
Gabriel Worang <gabriel.nathanael@student.umn.ac.id>
2026-03-06 21:14:46 +0700

    week04: (task 2) create parent class Employee"

---
+++ src/oop_00000106290_GabrielNathanaelWorang/week04/TugasMandiri2/Employee.kt
@@ -0,0 +1,12 @@
+package oop_00000106290_GabrielNathanaelWorang.week04.TugasMandiri2
+
+open class Employee(val name: String, val baseSalary: Int) {
+
+    open fun work() {
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int {
+        return (baseSalary * 0.1).toInt()
+    }
+}
